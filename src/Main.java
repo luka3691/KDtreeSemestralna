912d7b16122f4d596b2f344ea5e43db79c13928b
Luka Filadelfi <filadelfi@stud.uniza.sk>
2024-12-05 16:54:40 +0100

    Pridanie measuting with hash

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,13 @@
 import Hash.ExtendibleHash;
 import Hash.TestClassWithHash;
-import Tests.Measuring;
-import Tests.MeasuringWithHash;
-import Tests.Tester;
-import Tests.TesterHeap;
+import Tests.*;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
 
-        MeasuringWithHash measuring = new MeasuringWithHash();
+        MeasuringWithHashString measuring = new MeasuringWithHashString();
         //testovanie heap file
         //Tester tester = new Tester();
         //tester.insertData(30);
---
+++ src/Tests/MeasuringWithHashString.java
@@ -0,0 +1,116 @@
+package Tests;
+
+import Hash.BlockWithHash;
+import Hash.ExtendibleHash;
+import Hash.TestClassWithECVHash;
+import Hash.TestClassWithHash;
+
+import java.util.ArrayList;
+import java.util.Random;
+
+public class MeasuringWithHashString {
+    private int pocetOperacii = 50;
+    private int pocetData= 500;
+    private ExtendibleHash<TestClassWithECVHash> ecvTest;
+
+    ArrayList<TestClassWithECVHash> data;
+    Random random;
+    private int id;
+    private static final char[] CHARACTERS = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789".toCharArray();
+    private static final int BASE = CHARACTERS.length;
+    private static int counter = 0;
+    public MeasuringWithHashString() {
+        TestClassWithHash test = new TestClassWithHash();
+        TestClassWithECVHash test2 = new TestClassWithECVHash();
+        this.ecvTest = new ExtendibleHash<>(400, "DataECV.bin", "RiadiaceECV.bin", test2);
+        this.data = new ArrayList<>();
+        random = new Random(1);
+        id = 1;
+    }
+
+    public void insert() {
+        for (int i = 1; i < pocetData; i++) {
+            this.createData();
+        }
+        //System.out.println("Pocet vlozenych dat:" + pocetOperacii+ ". Počet dát v strome:" + this.tree.numberOfData());
+    }
+    private void createData() {
+        /*
+
+         */
+        TestClassWithECVHash test = new TestClassWithECVHash(id, getNextUniqueString(), id); //dopnit ked testovat
+        this.ecvTest.insert(test);
+        this.data.add(test);
+        if (this.ecvTest.get(test) == null) {
+            System.out.println("Error");
+        }
+        id++;
+    }
+
+    public void test() {
+        int pocetPokus = pocetOperacii;
+        int pocetDelete = 0;
+        int pocetInsert = 0;
+        //ArrayList<BlockWithHash<TestClassWithHash>> list = this.testHeap.getVsetkyBloky(this.data.getFirst());
+        for (int i = 0; i < pocetPokus; i++) {
+            switch (random.nextInt(1, 3)) {
+                case 1:
+                    System.out.println("Inserting");
+                    this.createData();
+                    pocetInsert++;
+                    break;
+                case 2:
+                    TestClassWithECVHash data = this.data.get(random.nextInt(0, this.data.size()-1));
+                    if (data.getECV().equals("AADE")) {
+                        System.out.println("TI");
+                    }
+                    if (this.ecvTest.get(data) == null) {
+                        System.out.println("Error");
+                    } else {
+                        System.out.println("Found");
+                    };
+                    break;
+                    /*
+                case 3:
+                    Pair<TestClass> dataNaDelete = this.data.get(random.nextInt(0, this.data.size()-1));
+                    pocetDelete++;
+                    boolean ret = this.testHeap.delete(dataNaDelete.adresa, dataNaDelete.data);
+
+                    if (! ret ) {
+                        System.out.println("Error deleting");
+                    }
+                    this.data.remove(dataNaDelete);
+
+                    if ( this.testHeap.get(dataNaDelete.adresa, dataNaDelete.data) != null) {
+                        System.out.println("Error: deleted data found");
+                    };
+                    break;
+
+
+                     */
+            }
+        }
+        this.ecvTest.close();
+    }
+
+    public static synchronized String getNextUniqueString() {
+        if (counter >= Math.pow(BASE, 4)) {
+            throw new IllegalStateException("Uz neexistuje viac.");
+        }
+        return toBase62(counter++);
+    }
+
+    private static String toBase62(int number) {
+        StringBuilder result = new StringBuilder();
+        do {
+            result.insert(0, CHARACTERS[number % BASE]);
+            number /= BASE;
+        } while (number > 0);
+
+        while (result.length() < 4) {
+            result.insert(0, CHARACTERS[0]); //vyplnennie
+        }
+
+        return result.toString();
+    }
+}
